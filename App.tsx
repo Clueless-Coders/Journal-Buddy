06e02d3a70710f276d3a62c7b47ce2e2a535bb31
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2024-02-16 13:39:49 -0600

    Showing Caroline some Git stuff

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import DailyPrompt from './src/Components/DailyPrompt'
 import Menu from './src/Components/HamburgerMenu/Menu'
 
-//This is a comment
 export default function App() {
   return (
       <SafeAreaView style={styles.wrapper}>
