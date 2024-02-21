bcee251172918d2be4fddd21b0fce041a681b6d3
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2024-02-21 12:29:19 -0600

    Basic setup for Journal Entries page

--- src/Components/Journal-Pages/JournalEntries.tsx
+++ src/Components/Journal-Pages/JournalEntries.tsx
@@ -1,25 +1,70 @@
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Platform, StatusBar, TextInput, FlatList } from 'react-native';
 import Menu from '../HamburgerMenu/Menu';
-import BackButton from '../Buttons/BackButton';
-import GeneralButtonDark from '../Buttons/GeneralButtonDark';
 import GeneralButtonLight from '../Buttons/GeneralButtonLight';
 
 export default function JournalEntries() {
     return (
         <SafeAreaView style={styles.overlord}>
-            <BackButton onPress={() => console.log('hello!')} buttonText='Hello!' />
-            <GeneralButtonDark onPress={() => console.log('hello!')} buttonText='Hello!' containerStyle={{height: 300, width: 500}} textStyle={{fontSize: 30, color: 'orange'}}/>
-            <GeneralButtonLight onPress={() => console.log('hello!')} buttonText='Hello!' containerStyle={{height: 20, width: 20}} textStyle={{fontSize: 12, color: 'red'}}/>
+            <View style={{zIndex: 1}}>
+                <Menu />
+            </View>
+            <View style={styles.container}>
+                <Text style={styles.headerText}>
+                    Journal Entries
+                </Text>
+                
+            </View>
+            <View style={styles.mainContent}>
+                <TextInput placeholder='Search' style={styles.inputBox}/>
+                <FlatList data={DATA} renderItem={
+                    ({item}) => 
+                        <GeneralButtonLight 
+                            onPress={() => console.log(item.toJSON())} 
+                            buttonText={item.toDateString()} 
+                            containerStyle={styles.containerStyle}
+                        />
+                }/>
+            </View>
+            
         </SafeAreaView>
         
     );
 }
 
+const DATA = [new Date(), new Date(), new Date(), new Date()];
+
 const styles = StyleSheet.create({
     overlord: {
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
         backgroundColor: 'white',
+        flex: 1,
+        
+    },
+    container: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        width: '80%',
+        borderBottomWidth: 2,
+        borderColor: 'grey'
+    },
+    headerText: {
+        fontSize: 35,
+        padding: 5
+    },
+    mainContent: {
+        alignItems: 'center',
         flex: 1
+    },
+    inputBox: {
+        borderWidth: .5,
+        width: '80%',
+        backgroundColor: '#f2f9ff',
+        padding: 5,
+        borderRadius: 25,
+        margin: 5
+    },
+    containerStyle: {
+        width: 315
     }
 });
