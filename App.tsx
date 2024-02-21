e90f85d3d25a85023769de4cb07bcbd5e97e9190
Caroline Peach <cpeach2@lsu.edu>
2024-02-20 18:49:52 -0600

    Setup

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import DailyPrompt from './src/Components/Journal-Pages/DailyPrompt';
 import { View } from 'react-native';
 import React from 'react';
+import LoginPage from './src/Components/Login/LoginPage';
 
 
 //TODO: Allow each page to change the currentPage state in order to switch which page is being displayed.
 //TODO: Create bottom taskbar
 export default function App() {
-  var [currentPageID, setCurrentPage] = React.useState(0);
+  var [currentPageID, setCurrentPage] = React.useState(1);
   let currentPage: React.JSX.Element = <View />;
 
   switch(currentPageID){
     case 0: currentPage = <DailyPrompt />; break;
+    case 1: currentPage = <LoginPage />; break;
     default: currentPage = <DailyPrompt />;
   }
   return (
