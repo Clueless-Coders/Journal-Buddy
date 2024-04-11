a16eb0072a90c6e82f0b5e4685c5528dcb9dc11f
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2024-04-10 22:32:37 -0500

    added createJournal

---
+++ src/Database.ts
@@ -0,0 +1,45 @@
+import { get, set, child, ref, getDatabase, push } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+
+export type UserData = {
+    habits: string[], //holds unique idents for habits
+    journals: string[],
+    firstSignIn: boolean
+};
+
+export type Journal = {
+   user: string, //unique ident for owner of this journal
+   entry: string,
+   dateWritten: Date
+};
+
+export type habit = {
+    daysToComplete: "sun" | "m" | "tu" | "w" | "th" | "f" | "sat",
+    timesToComplete?: {
+        hour: number,
+        minute: number,
+        afternoon: boolean //is in the afternoon?
+    }[], //can have multiple times of day to complete the task
+    title: string,
+    description?: string
+    uid: string //unique identifier for this specific habit
+    user: string, //unique ident for habit owner user
+    endDate: Date //time to stop habit
+};
+
+export function createUser(userID: string) {
+    const rootRef = ref(getDatabase());
+    const initData = {
+        habits: [] as string[],
+        journals: [] as string[],
+        firstSignIn: true
+    } as UserData;
+
+    set(child(rootRef, `/users/${userID}`), initData);
+}
+
+export function createJournal(journal: Journal) {
+    const rootRef = ref(getDatabase()); 
+    const journalUID = push(child(rootRef, `/journals/`), journal).key;
+    set(ref(getDatabase(), `/users/${getAuth().currentUser?.uid}/${journalUID}`), journalUID);
+}
