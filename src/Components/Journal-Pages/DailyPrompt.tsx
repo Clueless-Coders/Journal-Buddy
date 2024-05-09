f483ad54cbf9c2905274b2087c2372b7cb0f5966
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2024-05-08 22:42:58 -0500

    Cleanup on aisle 4

--- src/Components/Journal-Pages/DailyPrompt.tsx
+++ src/Components/Journal-Pages/DailyPrompt.tsx
@@ -17,12 +17,7 @@ export default function DailyPrompt({ navigation, route }: any) {
         const auth = getAuth();
         if(auth.currentUser === undefined || auth.currentUser === null)
             return;
-        console.log("today");
-        console.log(new Date().getUTCDate());
-        console.log("journal day");
-        console.log(new Date(journal.dayWritten).getUTCDate());
-
-
+        
         if(route.params !== undefined && new Date().getUTCDate() !== new Date(journal.dayWritten).getUTCDate()){
             console.log("Editing detected! Editing journal id: " + route.params.journalID);
             updateJournal(route.params.journalID, journal);
